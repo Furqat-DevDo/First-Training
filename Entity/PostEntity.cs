abf4e85a1864af54ff40015199e9cfb98dcee637
Furqat-Develop <furqata93@gmail.com>
2022-01-23 00:22:45 +0500

    version0.0

--- Entity/PostEntity.cs
+++ Entity/PostEntity.cs
@@ -25,16 +25,8 @@ namespace test1.Entity
                 Id = entity.ID,
                 Title = entity.Title,
                 Content = entity.Content,
-                BannerImage = getimage(entity.BannerImage)
-
             };
             return postmodel;
-        }
-        public static IFormFile getimage(byte[] data)
-        {
-            var stream = new MemoryStream(data);
-            IFormFile file = new FormFile(stream, 0, data.Length, "Raim.jpg", ".jpg");
-            return file;
         }
     }
 }
